d903cb29216f96f77c7c6dc54613fee9e8299b7c
Stephen Murphy <stephen@hypernaut.com>
2012-06-07 15:22:39 -0700

    added bootstrap build for gear-lib

---
+++ bootstrap.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+/*
+ * Bootstraps browser based Gear.js library tasks.
+ */
+var gear = require('gear');
+
+var files = [
+    //'vendor/uglify.js',
+    'lib/jsminify.js'
+];
+
+new gear.Queue({registry: new gear.Registry({dirname: __dirname + '/lib/'})})
+    .load(files)
+    .concat()
+    .tasks({
+        write: {task: 'write', options: 'build/gear-lib.js'},
+        minify: {task: 'jsminify'},
+        writeminify: {task: 'inspect'},//, options: 'build/gear-lib.min.js', requires: 'minify'},
+        join: {requires: ['write', 'writeminify']}
+    })
+    .run(function(err, results) {
+        if (err) {
+            console.error(err);
+        }
+    });
---
+++ build/gear-lib.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+(function(exports) {
+    if (typeof require !== 'undefined') {
+        var parser = require("uglify-js").parser,
+            pro = require("uglify-js").uglify;
+    }
+
+    /**
+     * Minify JS.
+     *
+     * @param options {Object} Minify options.
+     * @param blob {Object} Incoming blob.
+     * @param done {Function} Callback on task completion.
+     */
+    exports.jsminify = function(options, blob, done) {
+        var ast,
+            uglified = "";
+
+        try {
+            ast = parser.parse(blob.toString(), true);
+            uglified = pro.gen_code(ast, options);
+            done(null, new blob.constructor(uglified));
+        } catch (e) {
+            // Uglify doesn't usually give descriptive errors if the parser fails.
+            done('Minify failed, source file unparseable: ' + blob);
+        }
+    };
+})(typeof exports === 'undefined' ? this.tasks || (this.tasks = {}) : exports);
