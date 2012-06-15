e8bd21fa93b996298e72441dacf7d330b23a9c35
Stephen Murphy <stephen@hypernaut.com>
2012-06-15 15:03:43 -0700

    cleaned up bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -8,50 +8,42 @@
  * Bootstraps browser based Gear.js library tasks.
  */
 var gear = require('gear'),
-    handlebars = require('handlebars');
-
-var wrap = handlebars.compile(
-    "\n\ndefine('{{name}}', ['require', 'exports'{{#modules}}, '{{.}}'{{/modules}}], function(require, exports{{#input}}, {{.}}{{/input}}) {\n\n" +
-    "{{{result}}}\n\n" +
-    "});\n\n"
-);
-
-var helper = handlebars.compile(
-    "define('gear-lib', ['require', 'exports'{{#tasks}}, '{{.}}'{{/tasks}}], function(require, exports) {\n" +
-    "var tasks = [];\n" +
-    "{{#tasks}}tasks.push(require('{{.}}'));{{/tasks}}\n" +
-    "tasks.forEach(function(mod) {for (var task in mod) {exports[task] = mod[task];}});\n" +
-    "});\n\n"
-);
-
-var files = {
-    'vendor/csslint.js': {},
-    'vendor/less.js': {},
-    'vendor/jslint.js': {},
-    'vendor/uglify.js': {},
-    'vendor/handlebars.js': {},
-    'lib/csslint.js': {name: 'gear-csslint', modules: ['csslint'], task: true},
-    'lib/cssminify.js': {name: 'cssminify', modules: ['less'], task: true},
-    'lib/jslint.js': {name: 'gear-jslint', modules: ['jslint/lib/linter'], task: true},
-    'lib/jsminify.js': {name: 'jsminify', modules: ['uglify-js'], task: true},
-    'lib/handlebars.js': {name: 'gear-handlebars', modules: ['handlebars'], task: true}
-};
-
-var tasks = [];
-for (var task in files) {
-    if (files[task].task) {
-        tasks.push(files[task].name);
-    }
-}
+    handlebars = require('handlebars'),
+    wrap = handlebars.compile(
+        "\n\ndefine('{{name}}', ['require', 'exports'{{#modules}}, '{{.}}'{{/modules}}], function(require, exports{{#input}}, {{.}}{{/input}}) {\n\n" +
+        "{{{result}}}\n\n" +
+        "});\n\n"
+    ),
+    helper = handlebars.compile(
+        "define('gear-lib', ['require', 'exports'{{#tasks}}, '{{.}}'{{/tasks}}], function(require, exports) {\n" +
+        "var tasks = [];\n" +
+        "{{#tasks}}tasks.push(require('{{.}}'));{{/tasks}}\n" +
+        "tasks.forEach(function(mod) {for (var task in mod) {exports[task] = mod[task];}});\n" +
+        "});\n\n"
+    ),
+    files = {
+        'vendor/csslint.js': {},
+        'vendor/less.js': {},
+        'vendor/jslint.js': {},
+        'vendor/uglify.js': {},
+        'vendor/handlebars.js': {},
+        'lib/csslint.js': {name: 'gear-csslint', modules: ['csslint'], task: true},
+        'lib/cssminify.js': {name: 'cssminify', modules: ['less'], task: true},
+        'lib/jslint.js': {name: 'gear-jslint', modules: ['jslint/lib/linter'], task: true},
+        'lib/jsminify.js': {name: 'jsminify', modules: ['uglify-js'], task: true},
+        'lib/handlebars.js': {name: 'gear-handlebars', modules: ['handlebars'], task: true}
+    },
+    tasks = Object.keys(files).filter(function(file) {return files[file].task;});
 
 new gear.Queue({registry: new gear.Registry({dirname: __dirname + '/lib/'})})
     .read(Object.keys(files))
+    .load(helper({tasks: tasks}))
     .jslint({nomen: true, sloppy: true, white: true, vars: true, callback: function(blob) {
         //console.log(blob.name ? blob.name : 'inline', blob.jslint);
     }})
     .concat({callback: function(blob) {
         var obj = files[blob.name];
-        if (obj.name) {
+        if (obj && obj.name) {
             var vars = {result: blob.result, modules: []};
             Object.keys(obj).forEach(function(attr) {vars[attr] = obj[attr];});
             //Object.keys(vars.modules).forEach(function(attr) {vars.paths[attr] = obj[attr];});
@@ -59,14 +51,10 @@ new gear.Queue({registry: new gear.Registry({dirname: __dirname + '/lib/'})})
         }
         return blob.result;
     }})
-    .load(helper({tasks: tasks}))
-    .concat()
     .tasks({
         dev:     {task: ['write', 'build/gear-lib.js']},
-
         prodmin: {task: 'jsminify'},
         prod:    {requires: 'prodmin', task: ['write', 'build/gear-lib.min.js']},
-
         join:    {requires: ['dev', 'prod']}
     })
     .run(function(err, results) {
