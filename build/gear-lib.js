0e8ef0a6a7bc43865ee7cf7cd353e5334d660a4f
Stephen Murphy <stephen@hypernaut.com>
2012-08-28 13:58:23 -0700

    building new gear after csslint pull request

--- build/gear-lib.js
+++ build/gear-lib.js
@@ -24116,12 +24116,25 @@ var linter = require('csslint').CSSLint;
 exports.csslint = function(options, blob, done) {
     options = options || {};
 
-    var result = linter.verify(blob.result, options), // Cast buffer to string
-        errors = result.messages.map(function(err) {return err.message;}),
-        linted = errors ? new blob.constructor(blob, {csslint: errors}) : blob;
+    var result,
+        callback = options.callback,
+        errors,
+        linted;
+    
+    delete options.callback; //clear the callback from the options
 
-    done(options.callback ? options.callback(linted) : null, linted);
+    if (Object.keys(options).length === 0) {
+        //If no options, set to null to get csslint's defaults
+        options = null;
+    }
+
+    result = linter.verify(blob.result, options); // Cast buffer to string
+    errors = result.messages.map(function(err) {return err.message;});
+    linted = errors ? new blob.constructor(blob, {csslint: errors}) : blob;
+
+    done(callback ? callback(linted) : null, linted);
 };
+
 
 });
 
