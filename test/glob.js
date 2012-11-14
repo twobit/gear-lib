fea1da33d795da95aff4588db2df50b979cedd09
Stephen Murphy <stephen@hypernaut.com>
2012-11-13 17:03:38 -0800

    added new glob task to read files using wildcards

---
+++ test/glob.js
@@ -0,0 +1,26 @@
+var should = require('should'),
+    Queue = require('gear').Queue,
+    Registry = require('gear').Registry,
+    glob = require('../lib/glob').glob,
+    fixtures = {
+        options: {pattern: 'ind*.js'},
+        expected: 'index.js'
+    };
+
+describe('glob()', function() {
+    it('should glob files', function(done) {
+        glob(fixtures.options, [], function(err, results) {
+            results[0].name.should.equal(fixtures.expected);
+            done(err);
+        });
+    });
+    
+    it('should add Blobs to queue', function(done) {
+        new Queue({registry: new Registry({tasks: {glob: glob}})})
+            .glob(fixtures.options)
+            .run(function(err, results) {
+                results[0].name.should.equal(fixtures.expected);
+                done();
+            });
+    });
+});
